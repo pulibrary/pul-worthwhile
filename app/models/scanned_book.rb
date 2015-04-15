44c33a95efcf14eda167e78ef6275937acefdb03
Jon Stroop <jpstroop@gmail.com>
2015-04-15 10:24:08 -0400

    Customize scanned book create workflow

--- app/models/scanned_book.rb
+++ app/models/scanned_book.rb
@@ -2,7 +2,15 @@
 #  `rails generate worthwhile:work ScannedBook`
 class ScannedBook < ActiveFedora::Base
   include ::CurationConcern::Work
-  # include ::CurationConcern::WithBasicMetadata
   include Sufia::GenericFile::Metadata
-  validates_presence_of :title,  message: 'Your work must have a title.'
+
+  property :portion_note, predicate: ::RDF::URI.new(::RDF::DC.description), multiple: false
+  property :description, predicate: ::RDF::URI.new(::RDF::DC.abstract), multiple: false
+  property :access_policy, predicate: ::RDF::URI.new(::RDF::DC.accessRights), multiple: false
+  property :use_and_reproduction, predicate: ::RDF::URI.new(::RDF::DC.rights), multiple: false
+  property :src_metadata_id, predicate: ::RDF::URI.new('http://library.princeton.edu/terms/metadata_id'), multiple: false
+
+  validates_presence_of :src_metadata_id,  message: 'You must provide a source metadata id.'
+  validates_presence_of :access_policy,  message: 'You must choose an Access Policy statement.'
+  validates_presence_of :use_and_reproduction,  message: 'You must provide a use statement.'
 end
---
+++ spec/views/curation_concern/base/new.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'curation_concern/scanned_books/new.html.erb', type: :view do
+  @input_ids = [
+    'scanned_book_src_metadata_id',
+    'scanned_book_portion_note',
+    'scanned_book_description',
+    'scanned_book_access_policy',
+    'scanned_book_use_and_reproduction',
+    'visibility_open',
+    'visibility_registered',
+    'visibility_embargo',
+    'visibility_lease',
+    'visibility_restricted'
+  ]
+
+  let(:curation_concern) { ScannedBook.new }
+
+  before do
+    render file: 'curation_concern/scanned_books/new', locals: { curation_concern: curation_concern } 
+  end
+
+  @input_ids.each do |input_id|
+    it "has field for #{input_id}"  do
+      expect(rendered).to have_field (input_id)
+    end
+  end 
+end
