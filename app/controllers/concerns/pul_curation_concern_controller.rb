9c7c3a2fa66fc2feb93f42f27cee285bff955cfc
Eliot Jordan <eliotj@princeton.edu>
2015-04-16 11:44:45 -0400

    user does not have to accept agreement

--- app/controllers/concerns/pul_curation_concern_controller.rb
+++ app/controllers/concerns/pul_curation_concern_controller.rb
@@ -6,4 +6,16 @@ module PulCurationConcernController
     params.require(hash_key_for_curation_concern).permit!
   end
 
+  # Overriding to avoid cehcking acceptance of user agreement
+  def create
+    if actor.create
+      after_create_response
+    else
+      setup_form
+      respond_with([:curation_concern, curation_concern]) do |wants|
+        wants.html { render 'new', status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
